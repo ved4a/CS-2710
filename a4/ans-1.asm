7426704ea7a0551bc995ec84f88e21a9c94691d6
Vedaa Anand <work.vedaa@gmail.com>
2025-02-20 15:27:28 +0530

    feat: initialize all variables- n -> user input, sum, count -> 0

--- a4/ans-1.asm
+++ a4/ans-1.asm
@@ -6,4 +6,22 @@ sum: .word 0
 count: .word 0
 prompt: .asciiz "Enter a number: "
 
+.text
+.globl main
+main:
+# print prompt
+li $v0, 4
+la $a0, prompt
+syscall
 
+# read integer and store in 'n'
+li $v0, 5
+syscall
+sw $v0, n
+
+# initialize sum, count to 0
+li $t0, 0
+sw $t0, sum
+
+li $t1, 0
+sw $t1, count
