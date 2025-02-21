c20c7e6b521c25259b650f052107bf2b59b6fab0
Vedaa Anand <work.vedaa@gmail.com>
2025-02-21 19:43:02 +0530

    fix: branch conditions

--- a4/ans-3.asm
+++ a4/ans-3.asm
@@ -6,81 +6,75 @@ prompt3: .asciiz "The nth Fibonacci number is: "
 .text
 .globl main
 main:
-	li $a0, 4
-	la $a0, prompt1
-	syscall
-	
-	li $a0, 5
-	syscall
-	move $t0, $a0
-	
-	# base cases
-	beq $t0, 0, return_zero
-	beq $t0, 1, return_one
-	
-	# invalid input
-	blt $t0, 0, invalid_input
-	
-	# iterative step
-	li $t1, 0 # x = 0
-	li $t2, 1 # y = 1
-	li $t3, 1 # counter set to 1
-	Loop:
-		beq $t0, 1, return_fib
-		add $t4, $t1, $t2 # z = x + y
-		add $t1, $t2, 0 # x = y
-		add $t2, $t4, 0 # y = z
-		
-		add $t3, $t3, 1 # increment count
-		
-		j Loop
-		
-	return_fib:
-		li $a0, 4
-		la $a0, prompt3
-		syscall
-		
-		li $a0, 1
-		la $a0, ($t2) # return y
-		syscall
-		
-		li $a0, 10
-		syscall
-		
-	return_zero:
-		li $a0, 4
-		la $a0, prompt3
-		syscall
-		
-		li $a0, 1
-		la $a0, ($zero) # return 0
-		syscall
-		
-		# end
-		li $a0, 10
-		syscall
-		
-	return_one:
-		li $a0, 4
-		la $a0, prompt3
-		syscall
-		
-		li $a0, 1
-		la $a0, 1 # return 1
-		syscall
-		
-		# end
-		li $a0, 10
-		syscall
-		
-	invalid_input:
-		li $a0, 4
-		la $a0, prompt2
-		syscall
-		
-		j main
-		
-		
-		
-		
-		  		
+    li $v0, 4
+    la $a0, prompt1
+    syscall
+
+    li $v0, 5
+    syscall
+    move $t0, $a0 # n -> $t0
+
+    # check validity
+    blt $t0, 2, invalid_input
+
+    # base cases
+    beq $t0, 0, return_zero
+    beq $t0, 1, return_one
+
+    li $t1, 0  # x = 0
+    li $t2, 1  # y = 1
+    li $t3, 1  # counter = 1
+
+Loop:
+    beq $t3, $t0, return_fib
+
+    add $t4, $t1, $t2 # z = x + y
+    move $t1, $t2 # x = y
+    move $t2, $t4 # y = z
+
+    addi $t3, $t3, 1 # counter++
+    j Loop
+
+return_fib:
+    li $v0, 4
+    la $a0, prompt3
+    syscall
+
+    li $v0, 1
+    move $a0, $t2 # y is the fib #
+    syscall
+
+    # exit
+    li $v0, 10
+    syscall
+
+return_zero:
+    li $v0, 4
+    la $a0, prompt3
+    syscall
+
+    li $v0, 1
+    li $a0, 0
+    syscall
+
+    li $v0, 10
+    syscall
+
+return_one:
+    li $v0, 4
+    la $a0, prompt3
+    syscall
+
+    li $v0, 1
+    li $a0, 1
+    syscall
+
+    li $v0, 10
+    syscall
+
+invalid_input:
+    li $v0, 4
+    la $a0, prompt2
+    syscall
+
+    j main
