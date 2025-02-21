b1ebf25ab9720d39ce5bca5a67f624c888f8453e
Vedaa Anand <work.vedaa@gmail.com>
2025-02-21 18:12:41 +0530

    feat: user input and new var for n

--- a4/ans-3.asm
+++ a4/ans-3.asm
@@ -1,4 +1,5 @@
 .data
+n: .word 0
 prompt1: .asciiz "Enter a number n: "
 prompt2: .asciiz "Please enter a positive number.\n"
 prompt3: .asciiz "The nth Fibonacci number is: "
@@ -6,3 +7,11 @@ prompt3: .asciiz "The nth Fibonacci number is: "
 .text
 .globl main
 main:
+li $v0, 4
+la $a0, prompt1
+syscall
+
+li $v0, 5
+syscall
+sw $v0, n
+lw $t0, n
