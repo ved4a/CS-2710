72513a8254e4ae9ef22bcf7b2af092e40b7b84b3
Vedaa Anand <work.vedaa@gmail.com>
2025-02-21 18:17:12 +0530

    feat: implement positive check for n

--- a4/ans-3.asm
+++ a4/ans-3.asm
@@ -15,3 +15,26 @@ li $v0, 5
 syscall
 sw $v0, n
 lw $t0, n
+
+PositiveCheck:
+	# if n >= 2, start recursive step
+	bge $t0, 2, recurse
+	# if n = 1, go return 1
+	beq $t0, 1, return
+	# else, n < 1 so prompt again
+	li $v0, 4
+	la $a0, prompt2
+	syscall
+	
+	li $v0, 4
+	la $a0, prompt1
+	syscall
+	
+	li $v0, 5
+	syscall
+	sw $v0, n
+	lw $t0, n
+	
+	j PositiveCheck
+recurse:
+return: 
