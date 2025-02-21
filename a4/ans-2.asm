7b57b9b772db432ba9ff1918c88e9d60c049bb0c
Vedaa Anand <work.vedaa@gmail.com>
2025-02-21 17:25:44 +0530

    feat: add prompt msg if number is <=0

--- a4/ans-2.asm
+++ a4/ans-2.asm
@@ -2,7 +2,18 @@
 .data
 prompt1: .asciiz "Enter the first number: "
 prompt2: .asciiz "Enter the second number: "
+prompt3: .asciiz "Please enter a positive number.\n"
 
 .text
 .globl main
 main:
+# get first user input
+li $v0, 4
+la $a0, prompt1
+syscall
+
+# read value
+li $v0, 5
+syscall
+sw $v0, $t0	
+	
