4c52da0d0c9ad2000214139864d7af30308d7501
Vedaa Anand <work.vedaa@gmail.com>
2025-02-21 17:28:32 +0530

    feat: ensure inputted numbers are positive

--- a4/ans-2.asm
+++ a4/ans-2.asm
@@ -16,4 +16,48 @@ syscall
 li $v0, 5
 syscall
 sw $v0, $t0	
+
+# check if the inputted number is positive
+Loop:
+	bge $t0, 1, exit
+	li $v0, 4
+	la $a0, prompt3
+	syscall
+	
+	li $v0, 4
+	la $a0, prompt1
+	syscall
+	
+	li $v0, 5
+	syscall
+	sw $v0, $t0
+exit:
+
+# do the same for the next number
+# get second user input
+li $v0, 4
+la $a0, prompt2
+syscall
+
+# read value
+li $v0, 5
+syscall
+sw $v0, $t1
+
+Loop:
+	bge $t1, 1, exit
+	li $v0, 4
+	la $a0, prompt3
+	syscall
+	
+	li $v0, 4
+	la $a0, prompt2
+	syscall
+	
+	li $v0, 5
+	syscall
+	sw $v0, $t1
+exit:
+
+
 	
