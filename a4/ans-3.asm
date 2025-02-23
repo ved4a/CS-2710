bf0a1c95710a720e767f59a01f61a205d730171c
Vedaa Anand <work.vedaa@gmail.com>
2025-02-23 20:21:36 +0530

    fix: structure of code, lw -> move

--- a4/ans-3.asm
+++ a4/ans-3.asm
@@ -22,37 +22,33 @@ main:
 	# set variables x, y, counter
 	li $t1, 0 # x = 0
 	li $t2, 1 # y = 1
-	li $t3, 1 # counter = 0
+	li $t3, 2 # counter = 2, since fib(1) = 0, fib(2) = 1, etc
 	
 	check_n:
 		# check if counter < n	
-		blt $t3, $t0, fib_loop
-	
-		# return y
-		li $v0, 4
-		la $a0, result_prompt
-		syscall
-	
-		li $v0, 1
-		move $a0, $t2
-		syscall
-	
-		# end program
-		li $v0, 10
-		syscall
-	
+		bge $t3, $t0, return_result
 	fib_loop:
 		add $t4, $t1, $t2 # set z = x + y
 		move $t1, $t2 # x = y
 		move $t2, $t4 # y = z
 		
-		addi $t3, $t3, 1
+		addi $t3, $t3, 1 # counter++
+		j check_n
+	
+	return_result:
+		li $v0, 4
+		la $a0, result_prompt
+		syscall
+			
+		li $v0, 1
+		move $a0, $t2
+		syscall
+			
+		li $v0, 10
+		syscall	
 		
-		j check_n		
-
 	invalid_input:
 		li $v0, 4
 		la $a0, incorrect_input_prompt
 		syscall
-		
 		j main	
