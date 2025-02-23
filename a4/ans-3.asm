825a4bb69097e27c6bd992498f87862439d1864d
Vedaa Anand <work.vedaa@gmail.com>
2025-02-23 19:45:30 +0530

    feat: make prompts, check input validity

--- a4/ans-3.asm
+++ a4/ans-3.asm
@@ -1,9 +1,27 @@
 .data
 initial_prompt: .asciiz "Enter a number n >= 2 : "
-incorrect_input_prompt: .asciiz "Please enter a number >= 2."
+incorrect_input_prompt: .asciiz "Please enter a number >= 2.\n"
 result_prompt: .asciiz "The nth Fibonacci number is "
 
 .text
 .globl main
 main:
+	# ask prompt
+	li $v0, 4
+	la $a0, initial_prompt
+	syscall
+	
+	# get user input
+	li $v0, 5
+	syscall
+	lw $t0, $v0 # store n in $t0
+	
+	# check if n < 2
+	blt $t0, 2, invalid_input
 
+invalid_input:
+	li $v0, 4
+	la $a0, incorrect_input_prompt
+	syscall
+	
+	j main	
